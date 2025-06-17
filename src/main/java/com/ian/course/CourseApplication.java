4838dbbe8624c7cdc0ec54a3b4a98a5281de32d2
ian <ianjudo552@gmail.com>
2025-06-16 23:49:16 -0300

    Project Created

---
+++ src/main/java/com/ian/course/CourseApplication.java
@@ -0,0 +1,13 @@
+package com.ian.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ian/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ian.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
